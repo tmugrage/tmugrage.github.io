2d5fb45304af33adfd3abeb85d53e80d14c78508
Tim Mugrage <tmugrage@gmail.com>
2020-11-17 16:32:47 -0500

    111

---
+++ lbp/src/js/config.js
@@ -0,0 +1,131 @@
+export default {
+    "sets": [
+      {
+        "name": "Daily",
+        "items": [
+          "https://www.wunderground.com/weather/us/oh/new-carlisle/KOHNEWCA6",
+          "https://mail.google.com/mail/u/0/#inbox",
+          "https://feedly.com/browse",
+          "https://instagram.com",
+          "https://outlook.com",
+          "https://mail.protonmail.com/inbox",
+          "https://smile.amazon.com",
+          "http://192.168.33.100/",
+          "https://my.ooma.com/phone/voicemail",
+          "http://htmltetris.com",
+          "https://my.healthequity.com/Member/MemberTransactions.aspx"
+        ]
+      },
+      {
+        "name": "Social",
+        "items": [
+          "https://www.reddit.com",
+          "https://tweetdeck.twitter.com",
+          "https://www.facebook.com",
+          "https://www.linkedin.com/feed",
+          "https://web.whatsapp.com"
+        ]
+      },
+      {
+        "name": "Entertainment",
+        "items": [
+          "https://www.youtube.com",
+          "https://music.youtube.com",
+          "https://focusmusic.fm/",
+          "https://generative.fm/",
+          "https://github.audio/",
+          "https://open.spotify.com/collection/artists",
+          "https://music.amazon.com",
+          "https://www.audible.com/",
+          "https://stories.audible.com/discovery",
+          "https://plex.tv/servers/shared_servers/accept?invite_token=arAb8ukLMPDkjZY91RxU",
+          "https://pr.yusofyaghi.com  "
+        ]
+      },
+      {
+        "name": "hacking",
+        "items": [
+          "https://cad.onshape.com",
+          "https://www.thingiverse.com",
+          "https://www.prusaprinters.org",
+          "https://www.w3schools.com/python/default.asp",
+          "https://www.educative.io/courses/learn-python-from-scratch",
+          "https://github.com/tmugrage",
+          "https://gitlab.com/tmugrage",
+          "https://fuhrmann.github.io/keypirinha-theme-builder/",
+          "https://github.com/mbadolato/iTerm2-Color-Schemes#screenshots",
+          "https://typing.com",
+          "https://www.typingclub.com/  "
+        ]
+      },
+      {
+        "name": "mom estate",
+        "items": [
+          "https://account.mymedicare.gov",
+          "https://firstbankofcg.com/online",
+          "https://hotmail.com",
+          "https://hazlehurstnh.com",
+          "https://tools.usps.com/find-location.htm?location=1379247",
+          "https://www.libertygatax.com/taxes.html#/Record/A7509BEB84BF021FE8B5F95477209011",
+          "https://www.suzeorman.com/resources/record-keeping  "
+        ]
+      },
+      {
+        "name": "stationary",
+        "items": [
+          "https://cwpencils.com/",
+          "https://www.penaddict.com",
+          "https://www.penaddict.com/blog/category/Giveaways",
+          "https://www.wellappointeddesk.com/tag/giveaway",
+          "https://clickypost.com",
+          "http://karaskustoms.com",
+          "https://www.jetpens.com",
+          "http://onestarleathergoods.com",
+          "https://innovativejournaling.com/composition-leather-book-covers",
+          "https://dmleatherstudio.com/collections",
+          "https://www.growleyleather.com  "
+        ]
+      },
+      {
+        "name": "deskmods",
+        "items": [
+          "https://wallpaperscraft.com",
+          "https://unsplash.com",
+          "https://www.pexels.com",
+          "https://wallpapertag.com",
+          "https://awesomewallpapersblog.com",
+          "http://simpledesktops.com",
+          "https://www.wallpaperflare.com",
+          "http://wallpaperswide.com/latest_wallpapers.html",
+          "https://wallpaperaccess.com  "
+        ]
+      }
+    ],
+    "options": {
+      "darkMode": false,
+      "searchEngine": "https://www.google.com/search",
+      "faviconSize": "16",
+      "faviconService": "https://www.google.com/s2/favicons?domain=",
+      "linkTarget": "_blank",
+      "keyboardShortcuts": true
+    },
+    "plugins": [
+      {
+        "name": "weather",
+        "enabled": false,
+        "corsProxy": "",
+        "apiKey": "",
+        "locationAlias": "",
+        "latitude": "",
+        "longitude": "",
+        "lang": "en",
+        "units": "si"
+      },
+      {
+        "name": "age",
+        "enabled": false,
+        "birthday": "1970-06-21",
+        "goal": "100"
+      }
+    ]
+  };
