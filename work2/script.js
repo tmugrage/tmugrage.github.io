882d516c67b5384f10523bd11d45db79ae59adc4
Timothy Mugrage <tmugrage@live.com>
2020-12-17 15:33:02 -0500

    Update script.js

--- work2/script.js
+++ work2/script.js
@@ -3,7 +3,7 @@
 // Change this to your liking
 var username = "fellow Cisconian";
 // Counter that is incremented when the search engines are cycled through
-let se = 3;
+let se = 4;
 
 // Listens for click event in se_button, once clicked, se increments by one and cycleSearchEngines() is called to update the icon, placeholder, and form action
 document.getElementById("se_button").addEventListener("click", function() {
