e29fcb14b8626244f6a437a7a0161f7b97a2aec0
Timothy Mugrage <tmugrage@live.com>
2020-12-17 16:30:32 -0500

    Update script.js

--- work2/script.js
+++ work2/script.js
@@ -33,7 +33,8 @@ document.getElementById("go_btn").addEventListener("click", function(){
   if (document.getElementById("search").placeholder == "Searching with Cisco OneSearch") {
     console.log("Hello");
     document.getElementById("search").name = "queryFilter";
-  } else {document.getElementById("search").name = "q";
+  } else {
+    document.getElementById("search").name = "q";
   }
 }); 
 
