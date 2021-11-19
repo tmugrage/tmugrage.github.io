fd6830104c3b08da4fc23c148ce2a7e832363da9
Tim Mugrage <tmugrage@gmail.com>
2021-11-19 15:37:21 -0500

    1

--- bento/config.js
+++ bento/config.js
@@ -80,6 +80,12 @@ const CONFIG = {
       icon: 'youtube',
       link: 'https://odysee.com/',
     },
+    {
+      id: '7',
+      name: 'Odysee',
+      icon: 'youtube',
+      link: 'https://odysee.com/',
+    },
   ],
 
   // ┬  ┬┌─┐┌┬┐┌─┐
