bd292ac3a572eb46878a3b9970c0ccf660091dd3
Tim Mugrage <tmugrage@gmail.com>
2024-07-05 16:26:11 -0400

    commit the right code

--- personal/src/Providers/CustomCss.tsx
+++
@@ -1,6 +0,0 @@
-import { createStorageContext } from "@startpage/local-storage"
-
-import { initialCustomCss } from "./initialData"
-
-export const { StorageProvider: CustomCssProvider, useStorage: useCustomCss } =
-  createStorageContext("custom-css", initialCustomCss)
